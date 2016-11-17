838cbb9d891d02b0e4340ff5683d6fbe794f98ca
BustedSec <frank.trezza@gmail.com>
2016-11-17 15:16:15 -0500

    Update CheckNetShares.ps1

--- CheckNetShares.ps1
+++ CheckNetShares.ps1
@@ -5,5 +5,5 @@ Foreach ($PCname in $Computers)
    if (test-path $pcname)
         {$Pcname}
     else
-        {Send-MailMessage -SmtpServer autodiscover.ecinfosystems.com -From alerts@domain.com -To "address1@domain.com","address2@domain.com","address3@domain.com" -Subject "$PCname missing network shares" -Body $bod} 
+        {Send-MailMessage -SmtpServer mailserver.localdomain -From alerts@domain.com -To "address1@domain.com","address2@domain.com","address3@domain.com" -Subject "$PCname missing network shares" -Body $bod} 
 } 
